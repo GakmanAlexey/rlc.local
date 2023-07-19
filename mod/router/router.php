2e69df7cf95b69accd00a7116abe58f9aa2d5b39
GakmanAlexey <gakman@ya.ru>
2023-07-19 20:52:32 +0300

    Добален конструктор роутера

--- mod/router/router.php
+++ mod/router/router.php
@@ -3,6 +3,44 @@
 namespace Mod\Router;
 
 Class Router{
+    public static $h = [];
 
+    public function main($h){
+        self::$h = $h;
+        self::$h["Router"] = [];
+        self::$h["Router"]["status"] = "Работает";
+
+        if(self::$h["FireWall"]["connect_type"] == "user"){
+            self::$h = $this->user(self::$h);
+        }else if(self::$h["FireWall"]["connect_type"] == "bot"){
+            self::$h = $this->bot(self::$h);
+        }else if(self::$h["FireWall"]["connect_type"] == "cron"){
+            self::$h = $this->cron(self::$h);
+        }else{
+            self::$h = $this->error(self::$h);
+        }
+        return self::$h;
+    }
+
+    public function user($h){
+        
+
+        return $h;
+    }
+    public function bot($h){
+
+
+        return $h;
+    }
+    public function cron($h){
+
+
+        return $h;
+    }
+    public function error($h){
+        $h["error"]["Router"]["select_type"] = "Ошибка определения типа пользователя";
+
+        return $h;
+    }
    
 }
