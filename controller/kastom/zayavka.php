37456fc1dc6f682741d8bce8c899cd938f145196
GakmanAlexey <gakman@ya.ru>
2023-08-06 12:19:41 +0300

    Обновлена заявка

--- controller/kastom/zayavka.php
+++ controller/kastom/zayavka.php
@@ -12,14 +12,44 @@ Class Zayavka{
             $view = new \Mod\View\View;
             $view->show($h,$page);
 
+            $h= $this->read_date($h);
             $tg = new \Mod\Telegram\Telegram ;
-            $text="test";
-            $h = $tg->message($h,$text);
-            var_dump($_POST);
+            $h = $tg->message($h,$h["zayavka"]["text"]);
+            //var_dump($_POST);
         return $h;
     }
 
     public function read_date($h){
+        $name = $_POST["name"];
+        $gorod = $_POST["gorod"];
+        $phone = $_POST["phone"];
+        $age = $_POST["age"];
+        if(isset($_POST["possitions"])){
+            if($_POST["possitions"]=="kassir"){
+                $position ="Кассир";
+            }
+            else if($_POST["possitions"]=="povar"){
+                $position ="Повар";
+            }
+            else if($_POST["possitions"]=="cleaner"){
+                $position ="Уборка";
+            }else{
+                $position="Не выбрал";
+            }
+        }else{
+            $position ="Не выбрал";
+        }
+
+        $text = "
+        Откликнулся кандидат!
+        Имя: $name
+        Город: $gorod
+        Телефон: $phone
+        Позиция: $position
+------------";
+        $h["zayavka"] = [];
+        $h["zayavka"]["text"] = $text;
+        return $h;
 
     }
 };
