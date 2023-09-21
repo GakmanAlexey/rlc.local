43b2a2fe9f56ee8b9721996f66d1a0a15dc77863
GakmanAlexey <gakman@ya.ru>
2023-09-21 15:57:58 +0300

    Готова форма регистрации с бекендом

--- view/user/register.php
+++ view/user/register.php
@@ -4,11 +4,11 @@
         <div class="login_box_left">
             <p class="login_title">Регистрация</p>
             <form action="" class="form_login" method="post">
-                <input type="text" class="input_login" placeholder="Логин" name="login">
-                <input type="text" class="input_login" placeholder="Имя" name="name">
-                <input type="email" class="input_login" placeholder="Почта" name="mail">
-                <input type="password" class="input_login" placeholder="Пароль" name="password">
-                <input type="password" class="input_login" placeholder="Повторите пароль" name="password2">
+                <input type="text" class="<?php if(isset($h["error"]["user"]["login"])){echo "input_login_e\""." placeholder='".$h["error"]["user"]["login"]."'";}else{echo "input_login\" placeholder=\"Логин\"";}?>"  name="login">
+                <input type="text" class="<?php if(isset($h["error"]["user"]["name"])){echo "input_login_e\""." placeholder='".$h["error"]["user"]["name"]."'";}else{echo "input_login\" placeholder=\"Имя\"";}?>"   name="name">
+                <input type="email" class="<?php if(isset($h["error"]["user"]["mail"])){echo "input_login_e\""." placeholder='".$h["error"]["user"]["mail"]."'";}else{echo "input_login\" placeholder=\"Почта\"";}?>"  name="mail">
+                <input type="password" class="<?php if(isset($h["error"]["user"]["password"])){echo "input_login_e\""." placeholder='".$h["error"]["user"]["password"]."'";}else{echo "input_login\" placeholder=\"Пароль\"";}?>" name="password">
+                <input type="password" class="<?php if(isset($h["error"]["user"]["password2"])){echo "input_login_e\""." placeholder='".$h["error"]["user"]["password2"]."'";}else{echo "input_login\" placeholder=\"Повторите пароль\"";}?>" name="password2">
                 <button class="button_login" name="go" value="est">Продолжить</button>
             </form>
         </div>
@@ -20,6 +20,4 @@
     </div>
 </body>
 </html>
-<?php
-var_dump($_POST);
-?>
+
